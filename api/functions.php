47c6cadd2b6ee625e66dacabc91726d90e9333fe
Tyler Cote <tylercote@Tylers-MacBook-Pro.local>
2014-11-18 16:07:25 -0500

    added helpers.php to clean up functions.php

--- api/functions.php
+++ api/functions.php
@@ -1,61 +1,23 @@
 <?php
 
-function getConnection() {
-	include 'config.php';
-
-    $host     = $config['db_host'];
-    $username = $config['db_username'];
-    $password = $config['db_password'];
-    $table    = $config['db_table'];
-
-    $connection = mysqli_connect($host, $username, $password, $table);
-
-    return $connection;
-}
+include 'helpers.php';
 
 function getAllUsers(){
 		$query = "SELECT * FROM users";
-	    $connection = getConnection();
-
-        $result = mysqli_query($connection, $query);
-
-        $data = array();
-
-        while($row = mysqli_fetch_array($result)) {
-          $data[] = $row;
-        }
-
-        echo json_encode($data);
+        echo queryMultiple($query);
 }
 
 function getUsersByCategory($category){
 
 		$query = "SELECT * FROM users WHERE category = $category";
 
-        $connection = getConnection();
-
-        $result = mysqli_query($connection, $query);
-
-        $data = array();
-
-        while($row = mysqli_fetch_array($result)) {
-          $data[] = $row;
-        }
-
-        echo json_encode($data);
+        echo queryMultiple($query);
         
 }
 
 function getUserById($id){
 		$query = "SELECT * FROM users WHERE id = $id";
-
-        $connection = getConnection();
-
-        $result = mysqli_query($connection, $query);
-
-        $data = mysqli_fetch_array($result);
-
-        echo json_encode($data);
+        echo querySingle($query);
 }
 
 
---
+++ api/helpers.php
@@ -0,0 +1,41 @@
+<?php
+
+function getConnection() {
+    	include 'config.php';
+
+        $host     = $config['db_host'];
+        $username = $config['db_username'];
+        $password = $config['db_password'];
+        $table    = $config['db_table'];
+
+        $connection = mysqli_connect($host, $username, $password, $table);
+
+        return $connection;
+}
+
+function querySingle($query){
+        $connection = getConnection();
+
+        $result = mysqli_query($connection, $query);
+
+        $data = mysqli_fetch_assoc($result);
+
+        return json_encode($data);
+}
+
+function queryMultiple($query){
+        $connection = getConnection();
+
+        $result = mysqli_query($connection, $query);
+
+        $data = array();
+
+        while($row = mysqli_fetch_array($result)) {
+          $data[] = $row;
+        }
+
+        return json_encode($data);
+}
+
+
+?>
